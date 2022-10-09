13530a87b71e9a4064448a3ca18fd37a63f1e488
LeHoang <lehoang999113@gmail.com>
2022-10-09 16:26:28 +0700

    Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import './Btn.css';
-import './MUI.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './page/HomePage';
 import { Routes, Route } from "react-router-dom";
