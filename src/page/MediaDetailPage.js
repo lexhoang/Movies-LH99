38b3d0ee7e9fe5e86df7d279214b455952eabda2
LeHoang <lehoang999113@gmail.com>
2022-10-21 19:41:55 +0700

    Cast

--- src/page/MediaDetailPage.js
+++ src/page/MediaDetailPage.js
@@ -1,6 +1,7 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import MediaDetail from '../components/MediaDetail/MediaDetail';
+import Cast from '../components/MediaDetail/Cast';
 import RelatedMovies from '../components/MediaDetail/RelatedMovies';
 
 
@@ -9,6 +10,7 @@ function MediaDetailPage() {
         <div>
             <Header id="" />
             <MediaDetail />
+            <Cast />
             <RelatedMovies />
             <Footer />
         </div>
